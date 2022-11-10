1a19f22ce166512222482022c76848470bccde2a
QuickChangeSpidey <akshay.pandey@telus.com>
2022-11-10 13:59:55 -0500

    initial version commit

---
+++ Example/Pods/Target Support Files/Pods-uds-ios-theme-public-mobile_Example/Pods-uds-ios-theme-public-mobile_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_uds_ios_theme_public_mobile_Example : NSObject
+@end
+@implementation PodsDummy_Pods_uds_ios_theme_public_mobile_Example
+@end
---
+++ Example/Pods/Target Support Files/uds-ios-theme-public-mobile/uds-ios-theme-public-mobile-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_uds_ios_theme_public_mobile : NSObject
+@end
+@implementation PodsDummy_uds_ios_theme_public_mobile
+@end
